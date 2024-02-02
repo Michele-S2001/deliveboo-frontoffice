390e751069a3fa4412927fd02db3a12911aba385
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-02 16:47:22 +0100

    ADD > Front-office scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      page: 'App.vue'
+    }
+  }
+}
+
+
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style lang="scss">
+@use './styles/global.scss'
+
+</style>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data() {
+      return {
+        page: 'Home page di TasteIT'
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Qui va la home page: CAPRAAA!</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
