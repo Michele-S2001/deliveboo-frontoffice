f25456150102f4613954ecc6f5023d0df7ca1ba1
DamianoL <damianolunardo@hotmail.it>
2024-02-03 17:15:04 +0100

    Add - bootstrap

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import { router } from './router'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).mount('#app')
