303087377dec795fac65bcade5f053a2c599d377
DamianoL <damianolunardo@hotmail.it>
2024-02-05 14:22:39 +0100

    Revert "Update dratf header"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import { router } from './router'
 import App from './App.vue'
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).mount('#app')
