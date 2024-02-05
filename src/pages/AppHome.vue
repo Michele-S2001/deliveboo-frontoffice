20a64e15cf4af4bd701916662d860ad282695160
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-05 14:58:55 +0100

    ADD > Draft of categories

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,8 @@
 <script>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import AppHeader from '../components/AppHeader.vue';
+import store from '../store';
+import axios from 'axios';
 
 
 
@@ -12,8 +14,24 @@ export default {
 
   data() {
     return {
-      page: 'Home page di TasteIT'
+      page: 'Home page di TasteIT',
+      filter: [],
+      categories: null
     }
+  },
+
+  methods: {
+    fetchCategories() {
+      axios
+        .get(`${store.BASE_URL}/categories`)
+        .then((res) => {
+          this.categories = res.data.results;
+        })
+    }
+  },
+
+  created() {
+    this.fetchCategories();
   }
 }
 </script>
@@ -21,20 +39,18 @@ export default {
 <template>
   <DefaultLayout>
     <main class="content">
+      <!-- hero section -->
       <section class="hero">
-        <!-- Contenitore principale della pagina -->
         <div class="container">
-          <!-- Div con  ID 'wrapper' che avrà la classe 'show' solo se la variabile 'isExpanded' è true -->
-          <div id="wrapper">
-            <!-- Ciclo v-for che itera attraverso l'array 'categories' e crea un blocco per ciascuna categoria -->
+          <div class="wrapper">
             <div class="icon">
-              <!-- Input di tipo checkbox e una classe 'search' -->
-              <input type="checkbox" class="search">
-              <div class="circle">
-                <!-- Immagine che mostra l'icona della categoria. La sorgente dell'immagine è ottenuta dalla funzione getCategoryIcon -->
-                <img src="\img\biryani.png" alt="Category Icon" />
-              </div>
-              <!-- Nome della categoria -->
+              <input v-model="filter" type="checkbox" class="search" value="1">
+              <img src="\img\biryani.png" alt="Category Icon"/>
+              <span class="category-name">Italiano</span>
+            </div>
+            <div class="icon">
+              <input v-model="filter" type="checkbox" class="search" value="2">
+              <img src="\img\biryani.png" alt="Category Icon"/>
               <span class="category-name">Italiano</span>
             </div>
           </div>
@@ -46,58 +62,44 @@ export default {
 
 
 <style lang="scss" scoped>
-.wrapper {
-  margin: 30px 0;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-  justify-content: center;
+@use '../styles/partials/variables' as *;
 
-  .icon {
-    position: relative;
-    box-shadow: -4px 0px 0px 2px #f0b12e;
-    max-width: 50px;
+.hero {
+  background-color: $orange;
+  padding: 50px 0;
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
+
+    .icon {
+      position: relative;
+      box-shadow: -4px 0px 0px 2px #f0b12e;
+      background-color: white;
+      padding: 20px 25px;
+      border: 3px solid #e19f17;
+      border-radius: 56% 44% 45% 55%/58% 53% 47% 42%;
+
+      input[type="checkbox"] {
+        position: absolute;
+        inset: 0;
+        max-width: 80%;
+        margin: auto;
+        z-index: 10;
+        opacity: 0;
+      }
+    }
   }
-
-  &:hover {
-    transform: scale(1.1);
-  }
-
-  .circle {
-    background-color: white;
-    padding: 20px 25px;
-    border: 3px solid #e19f17;
-    border-radius: 56% 44% 45% 55%/58% 53% 47% 42%;
-  }
-
-  input[type="checkbox"] {
-    position: absolute;
-    inset: 0;
-    max-width: 80%;
-    margin: auto;
-    z-index: 10;
-    opacity: 0;
-  }
-}
-
-/*.hero {
-  background-color: #ffc244;
-}*/
-
-.container {
-  max-width: 964px;
-  margin: 0 auto;
-}
-
-img {
-  display: block;
-  max-width: 100%;
 }
 
 @media (min-width: 674px) {
-  .wrapper {
-    gap: 30px;
-    //justify-content: space-around;
+
+  .hero {
+    .wrapper {
+      gap: 30px;
+    }
+
   }
 }
 </style>
