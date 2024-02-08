cf5320b73cc80b8464be558ecba5db81edc4fafb
DamianoL <damianolunardo@hotmail.it>
2024-02-08 18:46:54 +0100

    Fix

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import AppMenu from './pages/AppMenu.vue';
 
-import AppCart from './pages/AppCart.vue';
+
 
 const router = createRouter({
   history: createWebHistory(),
