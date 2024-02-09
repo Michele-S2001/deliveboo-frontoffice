a6bb124b5cfcc2228919f8df3db5abb4ee6bb5b0
EmilianoAprile <emilianoaprile77@icloud.com>
2024-02-09 11:53:21 +0100

    Add - scroll to top on page

--- src/components/AppRestaurantCard.vue
+++ src/components/AppRestaurantCard.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="{ name: 'menu', params: { slug: currRestaurant.slug}}">
-    <div class="restaurant-card">
+    <div @click="scrollToTop" class="restaurant-card">
       <div class="restaurant-card__header">
         <img class="restaurant-img" :src="`http://127.0.0.1:8000/storage/${currRestaurant.thumb}`">
         <div class="categories">
@@ -18,7 +18,31 @@
   export default {
     props: {
       currRestaurant: Object
+    },
+    
+    methods: {
+    scrollToTop() {
+      const duration = 0.1; // Durata dello scorrimento in millisecondi
+      const start = window.scrollY; // Posizione di scorrimento verticale corrente della finestra
+      const distance = -start; // Distanza calcolata come l'opposto della posizione di scorrimento verticale corrente (verso l'alto)
+      let startTime = null;
+
+      const animation = (currentTime) => {
+        if (startTime === null) startTime = currentTime; // Tempo iniziale/corrente passato da requestAnimationFrame
+        const timeLapsed = currentTime - startTime;
+        const run = this.ease(timeLapsed, start, distance, duration); // Calcoliamo la posizione corrente di scorrimento utilizzando la funzione di easing, passando gli argomenti 
+        window.scrollTo(0, run); // Posizionare la finestra del browser verticalmente calcolato dall'ease
+        if (timeLapsed < duration) requestAnimationFrame(animation); // Se il tempo trascorso è inferiore alla durata dell'animazione, richiediamo il prossimo frame di animazione tramite requestAnimationFrame, passando la funzione animation 
+      };
+
+      requestAnimationFrame(animation);
+    },
+
+    ease(t, b, c, d) {
+      t /= d; // Normalizza il tempo t dividendo per la durata totale d / T varia da 0 a 1 durante l'intera durata dell'animazione.
+      return c * t * t + b;
     }
+  }
   }
 </script>
 
