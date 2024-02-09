315f9299ffd635744eb8e26ee6b1f20e71885bdc
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-09 18:07:10 +0100

    ADD > Static cart implementation

---
+++ src/components/AppEmptyCart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu-cart__empty" >
+    <img src="../../img/grocery-cart.png">
+    <p class="message">Ancora non hai aggiunto nulla al carrello, appena lo farai i prodotti e le quantità appariranno qui</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.menu-cart__empty {
+  img {
+    margin: 0 auto;
+    margin-bottom: 24px;
+    margin-top: 24px;
+  }
+  
+  .message {
+    text-align: center;
+    font-size: 21px;
+    padding: 20px 0;
+  }
+}
+
+</style>
