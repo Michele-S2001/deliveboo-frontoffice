f9537b3fad6a9211694f86ef0c8d92a69e44d3d8
DamianoL <damianolunardo@hotmail.it>
2024-02-09 16:26:20 +0100

    Add - category count, button new svg

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from './pages/AppHome.vue';
 import AppMenu from './pages/AppMenu.vue';
-import AppCart from './pages/AppCart.vue';
+import AppCart from './pages/AppFormCart.vue';
 
 const router = createRouter({
   history: createWebHistory(),
