3487e21139d40d35e886f3b99c79da5dd8353456
DeboraChiarelli <deborachiarelli92@gmail.com>
2024-02-12 18:24:15 +0100

    Add page 404

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import AppMenu from './pages/AppMenu.vue';
 import AppCart from './pages/AppFormCart.vue';
+import App404 from './pages/App404.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,17 +13,22 @@ const router = createRouter({
       name: 'home',
       component: AppHome
     },
-    
+
     {
-    path: '/menu/:slug',
-    name: 'menu',
-    props: true,
-    component: AppMenu
+      path: '/menu/:slug',
+      name: 'menu',
+      props: true,
+      component: AppMenu
     },
     {
       path: '/cart',
       name: 'cart',
       component: AppCart
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: App404
     },
   ]
 });
